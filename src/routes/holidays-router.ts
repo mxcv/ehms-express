c6d6d8eaf75452426a3bbb8ae6e2c424730004d7
Yevhen Kupriiuk <yevhenkupriiuk@gmail.com>
2024-03-11 18:30:36 +0200

    create function avoid duplicate logic

--- src/routes/holidays-router.ts
+++ src/routes/holidays-router.ts
@@ -3,32 +3,28 @@ import { holidayRequestRepository } from '../app.js';
 
 const router = express.Router();
 
+function changeStatus(req, res, newStatus) {
+    const requestId = Number(req.params.id);
+    try {
+        holidayRequestRepository.setStatus(requestId, newStatus);
+        res.redirect('/holidays');
+    } catch (error) {
+        console.error(`Error changing holiday request status to ${newStatus}:`, error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
 router.get('/', (req, res) => {
     const holidayRequests = holidayRequestRepository.getPending();
-
     res.render('holidays.ejs', { holidayRequests });
 });
 
 router.post('/:id/approve', (req, res) => {
-    const requestId = Number(req.params.id);
-    try {
-        holidayRequestRepository.setStatus(requestId, 'approved');
-        res.redirect('/holidays');
-    } catch (error) {
-        console.error('Error approving holiday request:', error);
-        res.status(500).send('Internal Server Error');
-    }
+    changeStatus(req, res, 'approved');
 });
 
 router.post('/:id/reject', (req, res) => {
-    const requestId = Number(req.params.id);
-    try {
-        holidayRequestRepository.setStatus(requestId, 'rejected');
-        res.redirect('/holidays');
-    } catch (error) {
-        console.error('Error rejecting holiday request:', error);
-        res.status(500).send('Internal Server Error');
-    }
+    changeStatus(req, res, 'rejected');
 });
 
 export default router;
