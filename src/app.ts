2165ff092d428ec849466ede4864a86e35bcddeb
Max <mxcs02@gmail.com>
2024-03-11 19:37:39 +0200

    Add logging middleware

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { HolidayManagmentRepositoryInMemoryFactory } from './repositories/reposi
 import employeeRouter from './routes/employee-router.js';
 import holidaysRouter from './routes/holidays-router.js';
 import addHolidayRouter from './routes/add-holiday-router.js';
+import util from 'util';
 
 dayjs.extend(utc);
 const repositoryFactory = new HolidayManagmentRepositoryInMemoryFactory();
@@ -12,18 +13,26 @@ const employeeRepository = repositoryFactory.createEmployeeRepository();
 const holidayRequestRepository = repositoryFactory.createHolidayRequestRepository();
 const holidayRulesRepository = repositoryFactory.createHolidayRulesRepository();
 const app = express();
+const port = 4000;
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
 
+app.use((req, res, next) => {
+    console.log(`${req.method}\t${req.url}\t${util.inspect(req.body)}`);
+    next();
+})
+
 app.get('/', (req, res) => res.render('index.ejs'))
 app.use('/employees', employeeRouter);
 app.use('/holidays', holidaysRouter);
 app.use('/add-holiday', addHolidayRouter);
 
-app.listen(4000);
+app.listen(port, () => {
+    console.log(`Listening on ${port}...`);
+});
 
 export {
     employeeRepository,
